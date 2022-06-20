cb4caa078ee41434050f4f85ac474c8f9217fd6d
Adil Reza <32259776+adilreza@users.noreply.github.com>
2022-06-21 00:01:07 +0600

    Time: 26 ms (34.76%), Space: 5.8 MB (73.93%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+    long long int temp, revers=0, rem;
+    temp = x;
+    if(x < 0 || (x != 0 && x % 10 == 0))
+        return false;
+    else {
+        while (1) {
+            if (x == 0) {
+                break;
+            } else {
+                rem = x%10;
+                revers = revers*10 + rem;
+                x = x/10;
+            }
+        }
+        if (temp==revers){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+};
